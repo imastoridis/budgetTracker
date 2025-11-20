98c2bb16c4c643cdb6b1b82399476934f9cd8f00
imasto <imastoridis@gmail.com>
2025-11-20 13:19:51 +0100

    Added BudgetTrackerApplication.java Added models User, Category, Transaction Added enums for Transaction Edited README

---
+++ src/main/java/com/budgetTracker/model/enums/TransactionType.java
@@ -0,0 +1,10 @@
+package com.budgetTracker.model.enums;
+
+/**
+ * Enumeration for the type of a financial transaction.
+ * Ensures that a transaction is always categorized as either income or expense.
+ */
+public enum TransactionType {
+    INCOME,
+    EXPENSE
+}
